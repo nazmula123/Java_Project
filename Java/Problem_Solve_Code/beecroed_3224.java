a6cc3e8b4b18e7b3aff40d788d4c863a9eca9c7e
Md : Nazmul Islam Hridoy <waheddr1@gmail.com>
2024-06-29 21:31:12 +0600

    Add files via upload

---
+++ Java/Problem_Solve_Code/beecroed_3224.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class beecroed_3224{
+    public static void main(String[]args){
+ Scanner sc=new Scanner(System.in);
+ String str1;
+ String str;
+ str1=sc.next();
+ str=sc.next();
+int a=str1.length();
+int b=str.length();
+if(a>=b)
+System.out.println("go");
+else System.out.println("no");
+
+
+    }
+}
