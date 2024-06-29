d2ee48930e791e4e0e311299065a43ccf4f32ca6
Md : Nazmul Islam Hridoy <waheddr1@gmail.com>
2024-06-29 21:32:49 +0600

    Add files via upload

---
+++ Java/Problem_Solve_Code/beecrowd_1097.java
@@ -0,0 +1,11 @@
+public class beecrowd_1097{
+    public static void main(String[]args){
+int a=7,b=5;
+ for(int i=1;i<=9;i=i+2){
+    for(int j=a;j>=b;j--){
+        System.out.println("I="+i+" J="+j);
+    }
+    a=a+2;
+    b=b+2;
+ }   }
+}
