5b0d2b129df7dd6715fcf1b784790f34bff2067a
Md : Nazmul Islam Hridoy <waheddr1@gmail.com>
2024-06-29 21:30:13 +0600

    Add files via upload

---
+++ Java/Problem_Solve_Code/beecrowd_1585.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class beecrowd_1585{
+    public static void main(String[]args){
+Scanner sc=new Scanner(System.in);
+int a;
+a=sc.nextInt();
+for(int i=0;i<a;i++){
+    int x=0,y=0;
+    x=sc.nextInt();
+    y=sc.nextInt();
+System.out.println((x*y)/2+" cm2");
+
+    }
+
+
+    }
+}
