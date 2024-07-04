32e3f11fa6b31ccb98f5953405c7961dd3a6d6e5
Md. Nazmul Islam Hridoy <waheddr1@gmail.com>
2024-07-05 00:16:57 +0600

    Add files via upload

---
+++ Java/Problem_Solve_Code/beecrowd_2950.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class beecrowd_2950{
+
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+double a,b,c;
+a=sc.nextDouble();
+b=sc.nextDouble();
+c=sc.nextDouble();
+System.out.printf("%.2f\n",a/(b+c));
+
+}
+}
